e5ce7de5fc29029565a86afc697b1344435f42a8
Ali-zayn <alizaynashaal@gmail.com>
2023-01-11 15:11:09 +0200

    estates info frontend

--- RentomaticCode/frontend/src/componets/Estates.jsx
+++ RentomaticCode/frontend/src/componets/Estates.jsx
@@ -6,6 +6,7 @@ import favorite2 from "../Images/favorite2.jfif";
 import favorite3 from "../Images/favorite3.jfif";
 import favorite4 from "../Images/favorite4.jfif";
 import favorite5 from "../Images/favorite5.jfif";
+import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
 
 function Estates() {
   return (
@@ -17,13 +18,157 @@ function Estates() {
           <h1>Recommended Places for you</h1>
           <div className="estate">
             <div className="estate-img">
-            <img src={favorite1} />
+              <img src={favorite1} />
             </div>
             <div className="estate-info">
-            <p>Private Villa in Beirut</p>
-          <h3>Deluxe Queen Room With Street View</h3>
-          <p>3 bedrooms / 2 bathrooms / Wifi / Kitchen </p>
-
+              <p>Private Villa in Beirut</p>
+              <h3>Deluxe Queen Room With Street View</h3>
+              <p>3 bedrooms / 2 bathrooms / Wifi / Kitchen </p>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarHalf />
+              </i>
+              <i>
+                <BsStar />
+              </i>
+              <div className="estate-price">
+                <p>2 Guests</p>
+                <h4>
+                  $ 100 <span>/ day</span>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="estate">
+            <div className="estate-img">
+              <img src={favorite2} />
+            </div>
+            <div className="estate-info">
+              <p>Private Villa in Beirut</p>
+              <h3>Deluxe Queen Room With Street View</h3>
+              <p>3 bedrooms / 2 bathrooms / Wifi / Kitchen </p>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarHalf />
+              </i>
+              <i>
+                <BsStar />
+              </i>
+              <div className="estate-price">
+                <p>2 Guests</p>
+                <h4>
+                  $ 100 <span>/ day</span>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="estate">
+            <div className="estate-img">
+              <img src={favorite3} />
+            </div>
+            <div className="estate-info">
+              <p>Private Villa in Beirut</p>
+              <h3>Deluxe Queen Room With Street View</h3>
+              <p>3 bedrooms / 2 bathrooms / Wifi / Kitchen </p>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarHalf />
+              </i>
+              <i>
+                <BsStar />
+              </i>
+              <div className="estate-price">
+                <p>2 Guests</p>
+                <h4>
+                  $ 100 <span>/ day</span>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="estate">
+            <div className="estate-img">
+              <img src={favorite4} />
+            </div>
+            <div className="estate-info">
+              <p>Private Villa in Beirut</p>
+              <h3>Deluxe Queen Room With Street View</h3>
+              <p>3 bedrooms / 2 bathrooms / Wifi / Kitchen </p>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarHalf />
+              </i>
+              <i>
+                <BsStar />
+              </i>
+              <div className="estate-price">
+                <p>2 Guests</p>
+                <h4>
+                  $ 100 <span>/ day</span>
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div className="estate">
+            <div className="estate-img">
+              <img src={favorite5} />
+            </div>
+            <div className="estate-info">
+              <p>Private Villa in Beirut</p>
+              <h3>Deluxe Queen Room With Street View</h3>
+              <p>3 bedrooms / 2 bathrooms / Wifi / Kitchen </p>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarFill />
+              </i>
+              <i>
+                <BsStarHalf />
+              </i>
+              <i>
+                <BsStar />
+              </i>
+              <div className="estate-price">
+                <p>2 Guests</p>
+                <h4>
+                  $ 100 <span>/ day</span>
+                </h4>
+              </div>
             </div>
           </div>
         </div>
